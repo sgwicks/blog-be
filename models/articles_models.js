636624fddc552e03ae587a0c5bf9da038d81add8
sgwicks <samgwicks@gmail.com>
2020-07-09 11:52:04 +0100

    Remove unneeded select modifiers from articles models

--- models/articles_models.js
+++ models/articles_models.js
@@ -3,13 +3,12 @@ const articlesRouter = require('../routers/articles')
 
 exports.fetchAllArticles = () => {
     return connection('articles')
-        .select('*')
+        .select()
         .then(articles => articles)
 }
 
 exports.fetchSingleArticle = (article_id) => {
     return connection('articles')
-        .select('*')
         .where('article_id', article_id)
         .then(article => article)
 }
